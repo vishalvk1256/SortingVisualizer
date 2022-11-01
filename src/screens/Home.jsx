24fd465f534c18a000d34df46fa0cb4de6135ea7
vishal_1256 <vishal.katta@eunimart.com>
2022-11-01 20:19:42 +0530

    changes

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -40,7 +40,7 @@ function Home() {
         }}
       >
         <ActionAreaCard
-          image="SortingVisualizer/sorting.png"
+          image="./SortingVisualizer/sorting.png"
           dummy="SortingVisualizer/boxsort.png"
           alt="BarGraphSort"
           title="BarGraph Sort"
@@ -49,7 +49,7 @@ function Home() {
           }
         />
         <ActionAreaCard
-          image="SortingVisualizer/boxsort.png"
+          image="./SortingVisualizer/boxsort.png"
           dummy="SortingVisualizer/boxsort.png"
           alt="BoxSort"
           title="Box Sort"
